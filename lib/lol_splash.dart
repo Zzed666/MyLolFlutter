5914d03b188603d994ab0f43a1190a0111d22aea
Zzed666 <2217520226@qq.com>
2019-10-08 22:21:30 +0800

    try to build a dart file of lol splash

---
+++ lib/lol_splash.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LolSplash extends StatefulWidget {
+  @override
+  _LolSplashState createState() => _LolSplashState();
+}
+
+class _LolSplashState extends State<LolSplash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/lol_splash.dart';
 
 void main()=>runApp(MyApp());
 
@@ -6,6 +7,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      home: LolSplash()
     );
   }
 }
