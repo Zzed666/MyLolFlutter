43119917690b4aa689abced43cd8a62e4c572aa3
sen.wan <sen.wan@neoderm.com.cn>
2019-10-09 18:39:26 +0800

    add splash modlue: 1,custom progress circle

---
+++ lib/lol_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LolHome extends StatefulWidget {
+  @override
+  _LolHomeState createState() => _LolHomeState();
+}
+
+class _LolHomeState extends State<LolHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(children: <Widget>[
+    ]));
+  }
+}
--- lib/lol_splash.dart
+++ lib/lol_splash.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/lol_home.dart';
+import 'package:flutter_app/customviews/custom_progress_circle.dart';
 
 class LolSplash extends StatefulWidget {
   @override
   _LolSplashState createState() => _LolSplashState();
 }
 
-class _LolSplashState extends State<LolSplash> {
+class _LolSplashState extends State<LolSplash>
+    with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 3000));
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+    _animationController.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => LolHome()),
+            (route) => route == null);
+      }
+    });
+    _animationController.forward();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+        child: Stack(children: <Widget>[
+      ConstrainedBox(
+          constraints: BoxConstraints.expand(),
+          child: FadeTransition(
+              opacity: _animation,
+              child: Image.asset("images/img_lol_splash.jpg",
+                  scale: 2.0, fit: BoxFit.cover))),
+      Positioned(
+          top: 30.0,
+          right: 10.0,
+          child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushAndRemoveUntil(
+                    MaterialPageRoute(builder: (context) => LolHome()), (route) => route == null);
+                _animationController.dispose();
+              },
+              child: SizedBox(
+                  width: 60.0,
+                  height: 60.0,
+                  child: CustomProgressCircleWidget())))
+    ]));
   }
 }
