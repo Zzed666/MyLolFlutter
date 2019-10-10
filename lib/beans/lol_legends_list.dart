8c24a569af90b6af3e83763e5b59cce5db133a82
sen.wan <sen.wan@neoderm.com.cn>
2019-10-10 19:28:09 +0800

    try to add a lol legends list in home modlue: 1,use dio to request http 2,use json_serializable to perform the response data 3,refresh and load more

---
+++ lib/beans/lol_legends_list.dart
@@ -0,0 +1,50 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'lol_legends_list.g.dart';
+
+
+@JsonSerializable()
+class lol_legends_list extends Object{
+
+  @JsonKey(name: 'code')
+  int code;
+
+  @JsonKey(name: 'msg')
+  String msg;
+
+  @JsonKey(name: 'datas')
+  List<Datas> datas;
+
+  lol_legends_list(this.code,this.msg,this.datas,);
+
+  factory lol_legends_list.fromJson(Map<String, dynamic> srcJson) => _$lol_legends_listFromJson(srcJson);
+
+}
+
+
+@JsonSerializable()
+class Datas extends Object{
+
+  @JsonKey(name: 'legendId')
+  String legendId;
+
+  @JsonKey(name: 'legendName')
+  String legendName;
+
+  @JsonKey(name: 'legendAge')
+  int legendAge;
+
+  @JsonKey(name: 'legendPosition')
+  String legendPosition;
+
+  @JsonKey(name: 'legendWords')
+  String legendWords;
+
+  @JsonKey(name: 'legendImageUrl')
+  String legendImageUrl;
+
+  Datas(this.legendId,this.legendName,this.legendAge,this.legendPosition,this.legendWords,this.legendImageUrl);
+
+  factory Datas.fromJson(Map<String, dynamic> srcJson) => _$DatasFromJson(srcJson);
+
+}
--- lib/lol_home.dart
+++ lib/lol_home.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_app/beans/lol_legends_list.dart';
 
 class LolHome extends StatefulWidget {
   @override
@@ -6,10 +11,85 @@ class LolHome extends StatefulWidget {
 }
 
 class _LolHomeState extends State<LolHome> {
+  List<Datas> _lolLegendsList = List();
+  ScrollController _scrollController;
+  bool isLoading = false;
+  bool loadMore = false;
+  int page = 0;
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener((){
+      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+        setState(() {
+          loadMore = true;
+        });
+        _getLolLegendsList();
+      }
+    });
+    loadMore = true;
+    _getLolLegendsList();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(children: <Widget>[
-    ]));
+        body: RefreshIndicator(
+            onRefresh: _getLolLegendsList,
+            child: ListView.builder(
+                itemCount: _lolLegendsList.length + 1,
+                itemBuilder: _itemWidget,
+                controller: _scrollController)));
+  }
+
+  Future _getLolLegendsList() async {
+    if (!isLoading) {
+      setState(() {
+        isLoading = true;
+      });
+      if(loadMore){
+        page++;
+      }else{
+        page = 1;
+        _lolLegendsList.clear();
+      }
+      Response _response = await Dio().get(
+          "https://www.fastmock.site/mock/14630744ec2c11c6b04a1c0638c9a839/firsttest/api/getLolLegendsList?page=$page");
+      setState(() {
+        _lolLegendsList.addAll(lol_legends_list.fromJson(_response.data).datas);
+        isLoading = false;
+        loadMore = false;
+      });
+    }
+  }
+
+  Widget _itemWidget(context, index) {
+    if (index < _lolLegendsList.length) {
+      return Container(
+          width: double.maxFinite,
+          height: 180.0,
+          child: Stack(children: <Widget>[
+            ConstrainedBox(
+                constraints: BoxConstraints.expand(),
+                child: Image.network(_lolLegendsList[index].legendImageUrl,
+                    fit: BoxFit.cover)),
+            Positioned(
+                left: 10.0,
+                top: 10.0,
+                child: Text(_lolLegendsList[index].legendName))
+          ]));
+    }
+    return Offstage(
+        offstage: !loadMore,
+        child: Center(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+              CircularProgressIndicator(strokeWidth: 1.0),
+              Text("加载中...")
+            ])));
   }
 }
