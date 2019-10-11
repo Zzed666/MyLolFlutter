dd7ddbd28afb9a82f81d008d6e36290c4a89e5c8
sen.wan <sen.wan@neoderm.com.cn>
2019-10-11 15:09:01 +0800

    try to add a lol legends list in home modlue: 5,item layout 6,add backgroud image

--- lib/lol_home.dart
+++ lib/lol_home.dart
@@ -1,8 +1,7 @@
-import 'dart:convert';
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'dart:ui';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart' as prefix0;
 import 'package:flutter_app/beans/lol_legends_list.dart';
 
 class LolHome extends StatefulWidget {
@@ -20,8 +19,9 @@ class _LolHomeState extends State<LolHome> {
   @override
   void initState() {
     _scrollController = ScrollController();
-    _scrollController.addListener((){
-      if(_scrollController.position.pixels == _scrollController.position.maxScrollExtent){
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
         setState(() {
           loadMore = true;
         });
@@ -36,12 +36,18 @@ class _LolHomeState extends State<LolHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: RefreshIndicator(
-            onRefresh: _getLolLegendsList,
-            child: ListView.builder(
-                itemCount: _lolLegendsList.length + 1,
-                itemBuilder: _itemWidget,
-                controller: _scrollController)));
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("images/img_lol_bg.jpg"),
+                    fit: BoxFit.cover)),
+            child: RefreshIndicator(
+                onRefresh: _getLolLegendsList,
+                child: ListView.builder(
+                    itemCount: _lolLegendsList.length + 1,
+                    itemBuilder: _itemWidget,
+                    controller: _scrollController))));
   }
 
   Future _getLolLegendsList() async {
@@ -49,21 +55,21 @@ class _LolHomeState extends State<LolHome> {
       setState(() {
         isLoading = true;
       });
-      if(loadMore){
+      if (loadMore) {
         page++;
-      }else{
+      } else {
         page = 1;
         _lolLegendsList.clear();
       }
       Response _response = await Dio().get(
           "https://www.fastmock.site/mock/14630744ec2c11c6b04a1c0638c9a839/firsttest/api/getLolLegendsList?page=$page");
       setState(() {
-        List<Datas> _lolLegendDatas = lol_legends_list.fromJson(_response.data).datas;
-        if(_lolLegendDatas != null) {
-          _lolLegendsList.addAll(lol_legends_list
-              .fromJson(_response.data)
-              .datas);
-        }else{
+        List<Datas> _lolLegendDatas =
+            lol_legends_list.fromJson(_response.data).datas;
+        if (_lolLegendDatas != null) {
+          _lolLegendsList
+              .addAll(lol_legends_list.fromJson(_response.data).datas);
+        } else {
           print("暂无数据");
         }
         isLoading = false;
@@ -74,19 +80,129 @@ class _LolHomeState extends State<LolHome> {
 
   Widget _itemWidget(context, index) {
     if (index < _lolLegendsList.length) {
-      return Container(
-          width: double.maxFinite,
-          height: 180.0,
-          child: Stack(children: <Widget>[
-            ConstrainedBox(
-                constraints: BoxConstraints.expand(),
-                child: Image.network(_lolLegendsList[index].legendImageUrl,
-                    fit: BoxFit.cover)),
-            Positioned(
-                left: 10.0,
-                top: 10.0,
-                child: Text(_lolLegendsList[index].legendName))
-          ]));
+      return Column(children: <Widget>[
+        Container(
+            alignment: Alignment.topLeft,
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              _lolLegendsList[index].legendName,
+              style: prefix0.TextStyle(fontSize: 16.0, color: Colors.white),
+              textAlign: TextAlign.left,
+            )),
+        Container(
+            width: double.maxFinite,
+            height: 180.0,
+            decoration: BoxDecoration(
+                border: Border(
+                    bottom: BorderSide(width: 1.0, color: Colors.black))),
+            child: Stack(children: <Widget>[
+              ConstrainedBox(
+                  constraints: BoxConstraints.expand(),
+                  child: Image.network(_lolLegendsList[index].legendImageUrl,
+                      fit: BoxFit.cover)),
+              //毛玻璃(蒙层:矩形剪辑->背景过滤->透明度)
+              Center(
+                  child: ClipRect(
+                      child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
+                          child: Opacity(
+                              opacity: 0.8,
+                              child: Container(
+                                  width: double.maxFinite,
+                                  height: double.maxFinite,
+                                  child: Row(children: <Widget>[
+                                    Align(
+                                        alignment: Alignment.centerLeft,
+                                        child: Container(
+                                            margin: EdgeInsets.only(left: 20.0),
+                                            decoration: BoxDecoration(
+                                                border: Border(
+                                                    top: BorderSide(
+                                                        color: Colors.black),
+                                                    right: BorderSide(
+                                                        color: Colors.black),
+                                                    bottom: BorderSide(
+                                                        color: Colors.black),
+                                                    left: BorderSide(
+                                                        color: Colors.black))),
+                                            constraints: BoxConstraints.expand(
+                                                width: 120.0, height: 120.0),
+                                            child: Image.network(
+                                                _lolLegendsList[index]
+                                                    .legendImageUrl,
+                                                fit: BoxFit.cover))),
+                                    Container(
+                                        width: 200.0,
+                                        margin: EdgeInsets.only(left: 10.0),
+                                        child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceAround,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: <Widget>[
+                                              RichText(
+                                                  text: TextSpan(
+                                                      style: TextStyle(
+                                                          color: Colors.green),
+                                                      text: "Age:",
+                                                      children: <TextSpan>[
+                                                    TextSpan(
+                                                        text:
+                                                            "${_lolLegendsList[index].legendAge}",
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.white))
+                                                  ])),
+                                              RichText(
+                                                  text: TextSpan(
+                                                      style: TextStyle(
+                                                          color: Colors.yellow),
+                                                      text: "Position:",
+                                                      children: <TextSpan>[
+                                                    TextSpan(
+                                                        text:
+                                                            "${_lolLegendsList[index].legendPosition}",
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.white))
+                                                  ])),
+                                              RichText(
+                                                  text: TextSpan(
+                                                      style: TextStyle(
+                                                          color:
+                                                              Colors.redAccent),
+                                                      text: "Words:",
+                                                      children: <TextSpan>[
+                                                    TextSpan(
+                                                        text:
+                                                            "${_lolLegendsList[index].legendWords}",
+                                                        style: TextStyle(
+                                                            color:
+                                                                Colors.white))
+                                                  ])),
+                                              FlatButton(
+                                                  child: Text("Select Skin",
+                                                      style: TextStyle(
+                                                          fontSize: 12.0)),
+                                                  textColor: Colors.white,
+                                                  disabledTextColor:
+                                                      Colors.blueGrey,
+                                                  color: Colors.lightBlue,
+                                                  disabledColor: Colors.grey,
+                                                  highlightColor: Colors.brown,
+                                                  splashColor: Colors.black12,
+                                                  padding: EdgeInsets.all(2.0),
+                                                  colorBrightness:
+                                                      Brightness.dark,
+                                                  shape: RoundedRectangleBorder(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              4.0)),
+                                                  onPressed: () {})
+                                            ]))
+                                  ]))))))
+            ]))
+      ]);
     }
     return Offstage(
         offstage: !loadMore,
