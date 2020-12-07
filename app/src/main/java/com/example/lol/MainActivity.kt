84c4c2023ff76a835ac678fc77909ce21a2ea3e9
zed <2217520226@qq.com>
2020-12-07 10:32:27 +0800

    modify flutter project into android project

---
+++ app/src/main/java/com/example/lol/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.lol
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testdemo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
